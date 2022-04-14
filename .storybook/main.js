ee8f2ec3c84a9c0bf422e419e53028d6745d1f7f
turusuke <mail@turusuke.com>
2022-04-14 18:04:51 +0900

    first commit

---
+++ .storybook/main.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+const toPath = (_path) => path.join(process.cwd(), _path);
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-a11y',
+    '@storybook/addon-interactions',
+  ],
+  features: {
+    interactionsDebugger: true, // 👈 enable playback controls
+  },
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@emotion/core': toPath('node_modules/@emotion/react'),
+          'emotion-theming': toPath('node_modules/@emotion/react'),
+        },
+      },
+    };
+  },
+};
